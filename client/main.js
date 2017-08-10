07917d0751657e49032316e17429ab6b77aa5423
Unknown <mkleinert@cemico.de>
2017-08-10 11:32:11 +0200

    React
    - Set dependencies
    - Inserting default HTML and JS at the client

--- client/main.js
+++ client/main.js
@@ -1,1 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Tracker } from 'meteor/tracker';
 
+Tracker.autorun(() => {
+
+});
+
+const jsx = (
+  <p>It works!</p>
+);
+
+Meteor.startup(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
