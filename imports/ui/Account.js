dc05b5a3136c8d9c16b285ae39f3c0621c9c1ddd
Maximilian Kleinert <mkleinert@cemico.de>
2017-08-10 17:16:45 +0200

    RemoveAccount
    - Function to remove accounts
    - Basic styles remove btn

--- imports/ui/Account.js
+++ imports/ui/Account.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+import { Accounts } from './../api/accounts';
+
 export default class Account extends React.Component {
   render() {
     return (
       <div className="account">
         <p>Name: {this.props.account.accountName}</p>
         <p>Passwort: {this.props.account.accountPassword}</p>
+        <button className="button button--round" onClick={() => Accounts.remove(this.props.account._id)}>X</button>
       </div>
     );
   }
